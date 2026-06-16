f79aef941b8e6dfa0cb981c4f0f5689ec724c198
sipul <leonyput1195@gmail.com>
2026-06-16 19:55:51 +0700

    First Trial: Print Hello and Data Types

---
+++ day1.py
@@ -0,0 +1,30 @@
+print("hello world!")
+hello world!
+print (3+1)
+4
+print (25% 100)
+25
+print (3/2)
+1.5
+print (25**25)
+88817841970012523233890533447265625
+print (3**2)
+9
+print (3//2)
+1
+print(type(10))
+<class 'int'>
+print(type(3.14))
+<class 'float'>
+print(type(1+3j))
+<class 'complex'>
+print(type('Leony'))
+<class 'str'>
+print (type([1,2,3]))
+<class 'list'>
+print(type({'name':'Leony'}))
+<class 'dict'>
+print(type({9.8,3.14,2.7}))
+<class 'set'>
+print(type((9.8,3.14,2.7)))
+<class 'tuple'>
